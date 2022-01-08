ac6fbe7fbe8f6b884061246ea9541e5752d048b8
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2022-01-08 21:13:31 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -47,8 +47,8 @@ async def youtube_dl_call_back(bot, update):
         )
         return False
     youtube_dl_url = update.message.reply_to_message.text
-    custom_file_name = <b> + str(response_json.get("title")) + \
-        "_" + youtube_dl_format + "." + youtube_dl_ext + </b> 
+    custom_file_name = + str(response_json.get("title")) + \
+        "_" + youtube_dl_format + "." + youtube_dl_ext  
     youtube_dl_username = None
     youtube_dl_password = None
     if "|" in youtube_dl_url:
